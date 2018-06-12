c79d11d2011d2db03f9af876533d7c5b8714edfc
suresh07 <soori_rozee@yahoo.com>
2018-06-12 18:16:15 +0530

    About page coppied from 28mm repo

--- application/views/flat/2-About/index.php
+++ application/views/flat/2-About/index.php
@@ -11,24 +11,26 @@
         <div class="col-md-9 main">
             <h1>About</h1>
 
-            <h2>83<sup>rd</sup> Annual Meeting - Indian Academy of Sciences</h2>
-            <p>The 83<sup>rd</sup> Annual Meeting of the Indian Academy of Sciences is being held at Shillong, hosted by the North-Eastern Hill University, during  3 &ndash; 5 November 2017. The two and a half days’ deliberation will see the participation of many Fellows and Associates of the Academy and invited teachers from different parts of the country who will engage in active scientific discussions with the Fellows.</p>
-            <p>The meeting will include inaugural lectures by newly elected Fellows and Associates over a wide range of scientific topics. The deliberations also include two special lectures on <i>Positive definite matrices</i> and <i>Targeted therapy for cancer</i>, along with  high-level scientific symposiam, on <i>New Technologies Changing Our Lives</i> and earth science of the North-East. Encompassing wider areas, the 83rd Annual Meeting will carry forward the tradition of public lectures by scholars from various fields.</p>
-            <p>The 83<sup>rd</sup> Academy meeting will also comprise many allied events including a teachers’ interaction session with the members of the Science Education Panel (SEP) and Fellows, Editorial Board meetings, an exhibition of Academy’s publications (journals and books), and a brief <i>press</i> conference.</p>
-            <p>All the scientific presentations are open to public, in particular college and university students, faculty and others.</p>        
+<p>
+    The year was 1989. The day, the second Saturday of the rain-soaked month of July. The Council members of the Indian Academy of Sciences were assembled in the Council Room of the Raman Research Institute. Prof. C N R Rao, as the president of the Academy, was chairing the meeting. The members of the Council felt that one Annual Meeting was not sufficient to accommodate all the newly elected Fellows of the Academy to present their work to the Fellowship and the general community. The necessity for a second meeting was deliberated. The following year, on Friday, the 27<sup>th</sup> of July, the Indian Academy of Sciences hosted its first Mid-Year Meeting at the Indian Institute of Science. 
+</p>
+<p>
+    Known for its diversity in scientific content delivered by highly acclaimed researchers, the Mid-Year meeting of the Indian Academy of Sciences is an eventful celebration of scientific accomplishments from across India. The Mid-Year Meetings – shorter and more intense than the Annual Meetings, but of the same ‘status’– continue to serve as a platform for the newly elected Fellows and Associates to present their inaugural talk covering the most recent of their scientific achievements.  
+</p>
+<p>
+    The 28<sup>th</sup> Mid-Year meeting was held on the 30<sup>th</sup> of June and the 1<sup>st</sup> of July, 2017, at the Faculty Hall of the Indian Institute of Science. Fellows of the Academy as well as Associates, invited teachers and students came together for a day and a half, participating in an exchange of knowledge and ideas. With talks on subjects ranging from caretaker tumour suppressors to quantum matter and nanomaterials-based light-harvesting systems to the unique properties of DNA replication in pathogens, the 28<sup>th</sup> Mid-Year was indeed an exciting occasion of learning exchange. 
+</p>
+<p>
+    The highlights of this meeting were the special lectures: Gagandeep Kang spoke on ‘Vaccines and Public Health in India’, highlighting the challenges India faces in the development and implementation of vaccines. Ullas Karanth, using examples from his three decades of ecological research on India’s national animal – the tiger, explored possible solutions for the challenges that ‘Conservation Science at Macro-Ecological Scales’ face in India.
+</p>
+<p>
+    The public lecture, held in the evening of the 30<sup>th</sup> June, focussed on ‘C. Subramania Bharati: Nationalism and Creativity’ by A.R. Venkatachalapathy. The symposium on Molecular Machines held on the 1<sup>st</sup>, provided insights into this fascinating field from the perspectives of physics, cell biology, structural biology and chemistry. 
+</p>
+<p>
+    Read the <i>Current Science</i> report of the 28th mid-year meeting <i><a href="http://www.currentscience.ac.in/Volumes/113/04/0547.pdf">here</a></i>.
+</p>    
 
-            <h2>Organizers</h2>
-            <p>
-                <img src="<?=PUBLIC_URL?>images/logo.png" alt="IAS Logo" style="float: left;width: 75px;margin: 10px 20px 50px 0"/>
-                <strong>Indian Academy of Sciences (IASc), Bengaluru:</strong> The Indian Academy of Sciences, as envisioned by its founder- Sir C V Raman, is devoted to the promotion and the upholding of the cause of science through diverse activities that aim to recognize and reward scientific efforts and talents. Science education programmes are also a major thrust area of the Academy. The Academy holds its annual meetings at different places of scientific interest in the country. The 83rd Annual Meeting of the Academy is being held at Shillong on the invitation of the Vice-Chancellor, North-Eastern Hill University. For more details visit: <a href="http://www.ias.ac.in" target="_blank">www.ias.ac.in</a>         
-            </p>
-           <h2>The Host:</h2>
-            <p>
-                <img src="<?=PUBLIC_URL?>images/stock/nehulogo.png" alt="North-Eastern Hill University Logo" style="float: left;width: 75px;margin: 0px 20px 150px 0"/>
-                <strong>North-Eastern Hill University</strong> was set up by an Act of Parliament and notified on 19th July 1973. The objectives of the University, as laid down in the act, are “to disseminate and advance knowledge by providing instructional and research facilities in such branches of learning as it may deem fit; to pay special attention to the improvement of the social and economic conditions and welfare of the people of the hill areas of the North-eastern region, and in particular, the intellectual, academic and cultural advancement.” 
-                <br /><br />
-                Popularly abbreviated as NEHU, the university stands tall in the northern part of Shillong in its green, lush picturesque and homely atmosphere. As one of the universities with potential for excellence, NEHU caters to students and scholars from almost all parts of the north-eastern region not to forget about students coming from other parts of the country and abroad. For more information, you can visit <a href="http://www.nehu.ac.in" target="_blank">www.nehu.ac.in</a>
-            </p>
+
         </div>
         <div class="col-md-3 sidebar">
 <?php require_once('application/views/generalSidebar.php');?>
