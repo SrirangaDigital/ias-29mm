cd1e9fb9bda414815876bdd393d75acc4500ae28
suresh07 <soori_rozee@yahoo.com>
2018-06-20 15:59:54 +0530

    Contact page updated

--- application/views/flat/5-Information/Contact.php
+++ application/views/flat/5-Information/Contact.php
@@ -10,9 +10,8 @@
     <div class="row">
         <div class="col-md-9 main">
             <h1>Contact information</h1>
-            <h2>For  general queries</h2>
-
             <div class="row">
+                <h2>For  general queries</h2>
                 <div class="col-md-6">
                     <strong>N. Maheshchandra</strong><br />
                     Executive Secretary<br />
@@ -30,6 +29,235 @@
                     Mob: 09008032002<br />
                     Fax: 080 2361 6094<br />
                     Email: ravi@ias.ac.in
+                </div>
+            </div>
+            <div class="row">
+                <h2>Registration of participants and management of information desk.</h2>
+                <div class="col-md-6">
+                    <strong>D L Geetha</strong><br />
+                    Email: dlgeetha@ias.ac.in<br />
+                    Mobile: 93798 95857<br /><br />
+                    <strong>R A Gracy Rani</strong><br />
+                    Email: gracy@ias.ac.in<br />
+                    Mobile: 97403 24185<br /><br />
+                    <strong>B R Nalini</strong><br />
+                    Email: nalini@ias.ac.in<br />
+                    Mobile: 98867 81301<br /><br />
+                    <strong>Jai Benjamin</strong><br />
+                    Email: joy@ias.ac.in<br />
+                    Mobile: 97436 36291
+                </div>
+                <div class="col-md-6">
+                    <strong>K S Shylaja</strong><br />
+                    Email: shylaja@ias.ac.in<br />
+                    Mobile: 99016 81773<br /><br />
+                    <strong>V Venkateswari</strong><br />
+                    Email: bhuvana@ias.ac.in<br />
+                    Mobile: 90087 54808<br /><br />
+                    <strong>Precilla</strong><br />
+                    Email: precilla@ias.ac.in<br />
+                    Mobile: 81475 52896<br /><br />
+                    <strong>Navya</strong><br />
+                    Email: navya@ias.ac.in<br />
+                    Mobile: 87626 21175
+                </div>
+            </div>
+            <div class="row">
+                <h2>Mid-Year Meeting Speakers’ affair and Scientific programme</h2>
+                <div class="col-md-6">
+                    <strong>M Srimathi</strong><br />
+                    Email: srimathi@ias.ac.in<br />
+                    Mobile: 98804 49261<br /><br />
+                    <strong>V Rajitha</strong><br />
+                    Email: rajitha@ias.ac.in<br />
+                    Mobile: 97312 11310
+                </div>
+                <div class="col-md-6">
+                    <strong>Mary J. Mathai</strong><br />
+                    Email: mary@ias.ac.in<br />
+                    Mobile: 99025 31781
+                </div>
+            </div>
+            <div class="row">
+                <h2>Public Lecture</h2>
+                <div class="col-md-6">
+                    <strong>K Nagesh</strong><br />
+                    Email: nagesh@ias.ac.in<br />
+                    Mobile: 98863 24220
+                </div>
+                <div class="col-md-6">
+                    <strong>Brahmananda NN</strong><br />
+                    Email: brahma@ias.ac.in<br />
+                    Mobile: 99862 25491
+                </div>
+            </div>
+            <div class="row">
+                <h2>Cultural Evening</h2>
+                <div class="col-md-6">
+                    <strong>T D Mahabaleswara</strong><br />
+                    Email: maha@ias.ac.in<br />
+                    Mobile: 94496 36215
+                </div>
+            </div>
+            <div class="row">
+                <h2>Editorial Board Meeting: Journal of Mathematical Sciences</h2>
+                <div class="col-md-6">
+                    <strong>M Srimathi</strong><br />
+                    Email: srimathi@ias.ac.in<br />
+                    Mobile: 98804 49261
+                </div>
+            </div>
+            <div class="row">
+                <h2>Editorial Board Meeting: Resonance</h2>
+                <div class="col-md-6">
+                    <strong>R Pushpavathi</strong><br />
+                    Email: pushpa@ias.ac.in<br />
+                    Mobile: 99450 07273
+                </div>
+                <div class="col-md-6">
+                    <strong>Geetha Sugumaran</strong><br />
+                    Email: geethas@ias.ac.in<br />
+                    Mobile: 81302 02933
+                </div>
+            </div>
+            <div class="row">
+                <h2>Council Meeting of the Academy</h2>
+                <div class="col-md-6">
+                    <strong>N. Maheshchandra</strong><br />
+                    Email: mahesh@ias.ac.in<br />
+                    Mobile: 90080 32004<br /><br />
+                    <strong>C S Ravikumar</strong><br />
+                    Email: ravi@ias.ac.in<br />
+                    Mobile: 90080 32002<br /><br />
+                    <strong>B R Nalini</strong><br />
+                    Email: nalini@ias.ac.in<br />
+                    Mobile: 98867 81301
+                </div>
+                <div class="col-md-6">
+                    <strong>A Shashidhar</strong><br />
+                    Email: shashidhar@ias.ac.in<br />
+                    Mobile: 99000 38761<br /><br />
+                    <strong>M Srimathi</strong><br />
+                    Email: srimathi@ias.ac.in<br />
+                    Mobile: 98804 49261
+                </div>
+            </div>
+            <div class="row">
+                <h2>Programme for the Spouses and Staff</h2>
+                <div class="col-md-6">
+                    <strong>Sushila Rajagopal</strong><br />
+                    Email: sushila@ias.ac.in<br />
+                    Mobile: 99000 94263
+                </div>
+                <div class="col-md-6">
+                    <strong>R Pushpavathi</strong><br />
+                    Email: pushpa@ias.ac.in<br />
+                    Mobile: 99450 07273
+                </div>
+            </div>
+            <div class="row">
+                <h2>Science Education Panel’s Symposium for teachers</h2>
+                <div class="col-md-6">
+                    <strong>C S Ravikumar</strong><br />
+                    Email: ravi@ias.ac.in<br />
+                    Mobile: 90080 32002<br /><br />
+                    <strong>K Geetha</strong><br />
+                    Email: geetha@ias.ac.in<br />
+                    Mobile: 81230 51203
+                </div>
+                <div class="col-md-6">
+                    <strong>M S Roopashri</strong><br />
+                    Email: roopa@ias.ac.in<br />
+                    Mobile: 87626 88980
+                </div>
+            </div>
+            <div class="row">
+                <h2>Science Education Panel’s Meeting</h2>
+                <div class="col-md-6">
+                    <strong>C S Ravikumar</strong><br />
+                    Email: ravi@ias.ac.in<br />
+                    Mobile: 90080 32002<br /><br />
+                    <strong>K Geetha</strong><br />
+                    Email: geetha@ias.ac.in<br />
+                    Mobile: 81230 51203
+                </div>
+                <div class="col-md-6">
+                    <strong>M S Roopashri</strong><br />
+                    Email: roopa@ias.ac.in<br />
+                    Mobile: 87626 88980
+                </div>
+            </div>
+            <div class="row">
+                <h2>Science Outreach activities by INYAS and the tAcT in association with Mysore Science Foundation</h2>
+                <div class="col-md-6">
+                    <strong>Shubhankar Biswas</strong><br />
+                    Email: trust@ias.ac.in<br />
+                    Mobile: 98862 42342
+                </div>
+            </div>
+            <div class="row">
+                <h2>Attending to the grievances of participants</h2>
+                <div class="col-md-6">
+                    <strong>T D Mahabaleswara</strong><br />
+                    Email: maha@ias.ac.in<br />
+                    Mobile: 94496 36215
+                </div>
+            </div>
+            <div class="row">
+                <h2>Reception at Bangalore airport</h2>
+                <div class="col-md-6">
+                    <strong>A Jayakumar</strong><br />
+                    Email: jaykumar@ias.ac.in<br />
+                    Mobile: 96327 26132
+                </div>
+                <div class="col-md-6">
+                    <strong>R P Vijay Kumar</strong><br />
+                    in consultation with Venkatarathnam N<br />
+                    Mobile: 94482 46083
+                </div>
+            </div>
+            <div class="row">
+                <h2>Accommodation</h2>
+                <div class="col-md-6">
+                    <strong>N. Maheshchandra</strong><br />
+                    Email: mahesh@ias.ac.in<br />
+                    Mobile: 90080 32004
+                </div>
+                <div class="col-md-6">
+                    <strong>Venkatarathnam N</strong><br />
+                    Email: venkat@ias.ac.in<br />
+                    Mobile: 90080 32003
+                </div>
+            </div>
+            <div class="row">
+                <h2>Catering</h2>
+                <div class="col-md-6">
+                    <strong>Chandrika Ramesh</strong><br />
+                    Email: chk@ias.ac.in<br />
+                    Mobile: 99806 90954<br /><br />
+                    <strong>Sudarshana Dhar</strong><br />
+                    Email: sdhar@ias.ac.in<br />
+                    Mobile: 99454 35868
+                </div>
+                <div class="col-md-6">
+                    <strong>N Padmaja</strong><br />
+                    Email: padmaja@ias.ac.in<br />
+                    Mobile: 94490 07041<br /><br />
+                    <strong>Rathnamma</strong><br />
+                    Mobile: 96635 10006
+                </div>
+            </div>
+            <div class="row">
+                <h2>TA/DA and other claims</h2>
+                <div class="col-md-6">
+                    <strong>A Shashidhar</strong><br />
+                    Email: shashidhar@ias.ac.in<br />
+                    Mobile: 99000 38761
+                </div>
+                <div class="col-md-6">
+                    <strong>B S Asha</strong><br />
+                    Email: asha@ias.ac.in<br />
+                    Mobile: 99454 58250
                 </div>
             </div>
         </div>
